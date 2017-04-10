5e1b180340528f7e6dcf5aad87d6ce33e83663d4
bworthin <brandy.worthington@gmail.com>
2017-04-10 16:37:24 -0400

    Initial setup

---
+++ Data_Wrang_Exercise1.R
@@ -0,0 +1,26 @@
+#Data Wrangling Exercise 1
+
+#Do I need to do anything else to store it as an object/data frame?
+
+
+#Clean up the 'company' column so all of the misspellings of the brand names
+#are standardized, same capitalization, etc.
+#???????
+
+#Separate product code and number into two new columns - 'product_code'
+#and 'product_number'
+#????
+
+
+#Add column 'product category' based on product code. p smartphone, v TV, x laptop, 
+#q tablet
+#????
+
+#Create a new column full_address that concatenates the three address fields
+#(address, city, country), separated by commas
+
+#Create four dummy binary variables for product and four for company
+#with the prefix company_ and product_
+
+
+
