d3eeadbda270b76c0545dc3a4047e618d4a1ae6a
bworthin <brandy.worthington@gmail.com>
2017-04-20 17:19:35 -0400

    Updated comments

--- ggplot2 Exercises/Titanic.R
+++ ggplot2 Exercises/Titanic.R
@@ -3,20 +3,20 @@
 # Check out the structure of titanic
 str(titanic)
 
-# Use ggplot() for the first instruction
+# Create a 'dodgy' bar chart of passengers by cPlass with fill by Sex
 ggplot(titanic, aes(x = factor(Pclass), fill = factor(Sex))) +
   geom_bar(position = "dodge")
 
 
-# Use ggplot() for the second instruction
+# Add a facet_grid layer to split into two graphs according to who survived vs didn't
 ggplot(titanic, aes(x = factor(Pclass), fill = factor(Sex))) +
   geom_bar(position = "dodge") +
   facet_grid(". ~ Survived")
 
-# Position jitter (use below)
+# Position jitter - set to 0.5
 posn.j <- position_jitter(0.5, 0)
 
-# Use ggplot() for the last instruction
+# Map Age to y axis, change to scatter plot, set jitter and alpha to handle overplotting more clearly
 ggplot(titanic, aes(x = factor(Pclass), y = Age, col = factor(Sex))) +
   geom_jitter(size = 3, alpha = 0.5, position = posn.j) +
   facet_grid(". ~ Survived")
